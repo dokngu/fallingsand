e07fdfb68e3a45e4e572c92710a8d45609f9dc7b
dokngu <dokngu@gmail.com>
2025-01-30 23:10:01 -0400

    Add color to "sand"

--- sketch.js
+++ sketch.js
@@ -12,18 +12,20 @@ function make2DArray(cols, rows) {
 let grid;
 let w = 10;
 let cols, rows;
+let hueValue = 2;
 
 function setup() {
     createCanvas(700, 700);
+    colorMode(RGB,215,169,55);
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = 0;
-        }
-    }
+    //
+    // for (let i = 0; i < cols; i++) {
+    //     for (let j = 0; j < rows; j++) {
+    //         grid[i][j] = 0;
+    //     }
+    // }
 
     grid[20][10] = 1;
 
@@ -40,16 +42,18 @@ function mouseDragged() {
             if (random(1) < 0.66) {
                 let col = mouseCol + i;
                 let row = mouseRow + j;
+                //check if cursor outside canvas
                 if (col > 3 * extent && col < cols - 5 * extent &&
                     row > 3 * extent && row < rows - 5 * extent) {
                     //console.log(col + ' ' + row);
-                    grid[col][row] = 1;
+                    grid[col][row] = hueValue;
                 }
             }
         }
     }
+    hueValue += 50; console.log(hueValue);
+    if(hueValue > 360) {hueValue = 2;}
 
-    //check if cursor outside canvas
 
 }
 
@@ -59,7 +63,8 @@ function draw() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             noStroke();
-            if (grid[i][j] === 1) {
+            if (grid[i][j] > 0) {
+                fill(grid[i][j], 169,55);
                 let x = i * w;
                 let y = j * w;
                 square(x, y, w);
@@ -71,7 +76,7 @@ function draw() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let state = grid[i][j];
-            if (state === 1) {
+            if (state > 0) {
                 let below = grid[i][j + 1];
                 let dir = random(1) > 0.5 ? 1 : -1;
 
@@ -85,13 +90,13 @@ function draw() {
 
                 if (below === 0) { // empty space beneath sand grain
                     nextGrid[i][j] = 0;
-                    nextGrid[i][j + 1] = 1; // sand grain moves to space below
+                    nextGrid[i][j + 1] = grid[i][j]; // sand grain moves to space below
                 } else if (belowA === 0) {
-                    nextGrid[i + 1][j + 1] = 1;
+                    nextGrid[i + 1][j + 1] = grid[i][j];
                 } else if (belowB === 0) {
-                    nextGrid[i - 1][j + 1] = 1;
+                    nextGrid[i - 1][j + 1] = grid[i][j];
                 } else { // sand grain beneath sand grain
-                    nextGrid[i][j] = 1; // stay where it is
+                    nextGrid[i][j] = grid[i][j]; // stay where it is
                 }
             }
         }
